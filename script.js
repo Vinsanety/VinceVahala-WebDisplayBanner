09d9bc6a45db53d92515398f07c4f5cf134002f5
vince vahala <vincevahala@gmail.com>
2024-04-12 12:06:18 -0700

    added scrolling behavior

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("Test");
+const scrollingContainer = document.querySelector(
+  ".scrolling-disclaimer_container"
+);
+
+const totalContentHeight = scrollingContainer.scrollHeight;
+
+const duration = 12000;
+
+let startTime;
+
+const delay = 13000;
+
+setTimeout(() => {
+  requestAnimationFrame(scrollToBottom);
+}, delay);
+
+function scrollToBottom(timestamp) {
+  if (!startTime) {
+    startTime = timestamp;
+  }
+
+  const elapsedTime = timestamp - startTime;
+
+  const scrollPosition = Math.min(
+    totalContentHeight,
+    (elapsedTime / duration) * totalContentHeight
+  );
+
+  scrollingContainer.scrollTop = scrollPosition;
+
+  if (elapsedTime < duration) {
+    requestAnimationFrame(scrollToBottom);
+  }
+}
